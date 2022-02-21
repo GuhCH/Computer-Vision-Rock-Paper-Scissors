e29648af3899f448517b09a26b7127f35bd43d1f
Joe Gocher <56391275+GuhCH@users.noreply.github.com>
2022-02-21 18:30:39 +0000

    Rock, Paper, Scissors game played with webcam.
    This version has a rough graphical display for the computer.
    
    (this commit is so large because i was working on a local repo that I accidentally deleted but managed to recover the code)

---
+++ RPS_With_Webcam.py
@@ -0,0 +1,91 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+import time
+
+# Runs the Rock, Paper, Scissors game, input is human's answer, output is score change + win/lose/draw msg
+def playRPS(answer):
+    states = ["rock","paper","scissors"]
+    imgStates = ["rock.jpg","paper.jpg","scissors.jpg"]
+    comp = np.random.randint(3)
+    if answer > 2:
+        return [0,0], "Invalid answer, try again!", cv2.imread(imgStates[comp])
+    elif answer-comp == 1 or answer-comp == -2:
+        return [1,0], "Your " + states[answer] + " beats my " + states[comp] + ". You win this round!", cv2.imread(imgStates[comp])
+    elif answer == comp:
+        return [0,0], "We both play " + states[answer] + ". It's a draw!", cv2.imread(imgStates[comp])
+    else:
+        return [0,1], "My " + states[comp] + " beats your " + states[answer] + ". You lose this round!", cv2.imread(imgStates[comp])
+
+# Displays the camera and runs the TensorFLow model
+def readModel(model,cap,data,comp_image):
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    frame1 = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_AREA)
+    comp_image1 = cv2.resize(comp_image, (640, 480), interpolation = cv2.INTER_AREA)
+    full_img = np.hstack((frame1, comp_image1))
+    cv2.imshow('frame', full_img)
+
+    # If model has >80% confidence in an answer, returns argument of that answer
+    if np.max(prediction) > 0.8:
+        return np.argmax(prediction)
+    # Else returns argument of null answer
+    else: return 3
+
+# Displays the camera without running TF model to save processing power
+def readCam(cap,comp_image):
+    ret, frame = cap.read()
+    frame1 = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_AREA)
+    comp_image1 = cv2.resize(comp_image, (640, 480), interpolation = cv2.INTER_AREA)
+    full_img = np.hstack((frame1, comp_image1))
+    cv2.imshow('frame', full_img)
+
+# Counts for 4 seconds, saying "Rock, Paper, Scissors, Shoot" 
+# Returns false if quit button is pressed in this time so round does not go ahead
+def countdown(cap):
+    words = ["rock.jpg","paper.jpg","scissors.jpg","shoot.png"]
+    for i in range(4):
+        comp_image = cv2.imread(words[i])
+        t0 = time.clock_gettime(time.CLOCK_BOOTTIME)
+        while time.clock_gettime(time.CLOCK_BOOTTIME)-t0 < 1: 
+            readCam(cap,comp_image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return False
+    return True
+
+model = load_model('keras_model.h5')
+cap = cv2.VideoCapture(0)
+comp_image = cv2.imread('wait_screen.png')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+score = [0,0]
+our_pronouns = ["You ","I "]
+while np.max(score) < 3:
+    readCam(cap,comp_image)
+    if cv2.waitKey(1) & 0xFF == ord('p'):
+        if countdown(cap) == True:
+            answer = readModel(model,cap,data,comp_image)
+            scoreChange, out_msg, comp_image = playRPS(answer)
+            print(out_msg)
+            if out_msg != "Invalid answer, try again!":
+                score[0] += scoreChange[0]
+                score[1] += scoreChange[1]
+                print("The score is now:")
+                print("You: " + str(score[0]))
+                print("Me: " + str(score[1]))
+                t0 = time.clock_gettime(time.CLOCK_BOOTTIME)
+                while time.clock_gettime(time.CLOCK_BOOTTIME)-t0 < 2: 
+                    readCam(cap,comp_image)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+            comp_image = cv2.imread('wait_screen.png')
+        else: break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+if np.max(score) == 3:
+    print(our_pronouns[np.argmax(score)] + "win!")
+else: print("Game cancelled.")
+print("Goodbye!")
